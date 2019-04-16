d8caaaf160aab2ae1fd1f7e2a9834a787acdb6d0
Shardul Kulkarni <ShardulMKulkarni@gmail.com>
2019-04-15 21:33:25 -0500

    Updates

---
+++ precache-manifest.a723aa85e5596a07dd366cd39ca430d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "26ead58317a649cdf406",
+    "url": "/tic-tac-toe/static/js/runtime~main.26ead583.js"
+  },
+  {
+    "revision": "365729aa00bdbf852d26",
+    "url": "/tic-tac-toe/static/js/main.365729aa.chunk.js"
+  },
+  {
+    "revision": "8649b2a3c146a3d374b2",
+    "url": "/tic-tac-toe/static/js/1.8649b2a3.chunk.js"
+  },
+  {
+    "revision": "365729aa00bdbf852d26",
+    "url": "/tic-tac-toe/static/css/main.5b7c3fea.chunk.css"
+  },
+  {
+    "revision": "840d979af6bd15857da770f3c71df9cc",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
